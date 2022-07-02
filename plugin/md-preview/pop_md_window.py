7b59ffd5205465324cea273c6743282cd3fe914a
traversalnat <1914619958@qq.com>
2022-07-03 01:18:20 +0800

    fix(pop_md_window.py): index_file error

--- plugin/md-preview/pop_md_window.py
+++ plugin/md-preview/pop_md_window.py
@@ -6,8 +6,7 @@ def pop_md_window(popweb, module_name, x, y, frame_x, frame_y, frame_w, frame_h,
     window_width = frame_w * width_scale
     window_height = frame_h * height_scale
     web_window.loading_js_code = ""
-    index_html = open("md.html", "r").read().replace("INDEX_DIR", os.path.dirname(index_file)).replace("MARKDOWN", md_string)
-    print(index_html)
+    index_html = open(index_file, "r").read().replace("INDEX_DIR", os.path.dirname(index_file)).replace("MARKDOWN", md_string)
     web_window.webview.setHtml(index_html, QUrl("file://"))
     web_window.resize(int(window_width), int(window_height))
     web_window.move(int(x), int(y))
